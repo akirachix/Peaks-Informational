734dad5db5b79a9229e3f8c53ac32803a142d372
Nataly-Wambui <wambuinatalygakenye@gmail.com>
2024-07-02 13:02:42 +0300

    Updates styling

--- taka-reverse/src/Contacts/index.js
+++ taka-reverse/src/Contacts/index.js
@@ -18,9 +18,6 @@ function Contact(){
             <span><BsFillTelephoneFill size= '2.0rem' color='#FFFFFF' /></span>
                 <p>+254 793 244 456</p>
                 <p>+254 786 965 766</p>
-                <p>+254 742 052 402</p>
-                <p>+254 704 264 110</p>
-                <p>+254 712 345 678</p>
             </div>
             <div>
                 <span><FaMapMarkerAlt size= '2.0rem' color='#FFFFFF'/></span>
@@ -31,13 +28,11 @@ function Contact(){
                 <p>akwangdeu@gmail.com</p>
                 <p>natalywambui@gmail.com</p>
                 <p>kantaimercy@gmail.com</p>
-                <p>ivywanjiku@gmail.com</p>
-                <p>maureenmwendwa@gmail.com</p>
             </div>
             </div>
             <hr/>
             <div className='url'>
-                <p>@2023reversetaka.com</p>
+                <p>@2024reversetaka.com</p>
             </div>
         </div>
     );
